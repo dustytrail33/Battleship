6afc03fd6f9c1889ef961b72b0dc8c687b33da78
dustytrail33 <dustytrail33@mail.ru>
2025-05-17 14:16:38 +0300

    feat: added environment

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+export default {
+  mode: 'production',
+  entry: {
+    dbServer: './src/index.ts',
+  },
+  target: 'node',
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+};
